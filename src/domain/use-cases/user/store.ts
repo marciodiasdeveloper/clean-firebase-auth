5cb637af7d3e00d0960f4d8ead5ba429e2ae7901
marciodias <marciodias@pop-os.localdomain>
2024-10-24 19:59:31 -0300

    fix: create firebase user

--- src/domain/use-cases/user/store.ts
+++ src/domain/use-cases/user/store.ts
@@ -1,6 +1,7 @@
 import { AuthEmailAlreadyInUse } from '@/application/errors'
 import { UserStore } from '@/domain/contracts/gateways'
 import { getAuth, createUserWithEmailAndPassword, UserCredential } from 'firebase/auth'
+import { getFirestore, doc, setDoc } from 'firebase/firestore'
 
 type Setup = (firebase: any) => UserStoreUseCase
 type Input = UserStore.Input
@@ -10,15 +11,36 @@ export type UserStoreUseCase = (input: Input) => Promise<Output>
 
 export const setupUserStore: Setup = ({ firebase }) => async input => {
   const auth = getAuth(firebase)
+  const db = getFirestore(app) // Atualiza o perfil do usuário com nome
+  // await updateProfile(user, {
+  //   displayName,
+  //   phoneNumber // Firebase Auth não armazena phoneNumber diretamente
+  // })
+
+  // Armazena informações adicionais no Firestore
+  // await setDoc(doc(db, "users", user.uid), {
+  //   email: user.email,
+  //   displayName: displayName,
+  //   phoneNumber: phoneNumber,
+  //   createdAt: new Date().toISOString()
+  // });
 
   try {
     const user: UserCredential = await createUserWithEmailAndPassword(auth, input.email, input.password)
 
     // Atualiza o perfil do usuário com nome
-    // await updateProfile(user, {
-    //   displayName,
-    //   phoneNumber // Firebase Auth não armazena phoneNumber diretamente
-    // })
+    await updateProfile(user, {
+      displayName,
+      phoneNumber // Firebase Auth não armazena phoneNumber diretamente
+    })
+
+    // Armazena informações adicionais no Firestore
+    await setDoc(doc(db, 'users', user.uid), {
+      email: user.email,
+      displayName,
+      phoneNumber,
+      createdAt: new Date().toISOString()
+    })
 
     return user
   } catch (error: any) {
