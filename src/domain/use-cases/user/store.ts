587320c0bbe19d8125b85e1636c3eb04b0fd39ff
marciodias <marciodias@pop-os.localdomain>
2024-10-24 16:44:56 -0300

    feat: implement user store controller and use case

--- src/domain/use-cases/user/store.ts
+++ src/domain/use-cases/user/store.ts
@@ -1,4 +1,5 @@
 import { UserStore } from '@/domain/contracts/gateways'
+import { getAuth, createUserWithEmailAndPassword, UserCredential } from 'firebase/auth'
 
 type Setup = (firebase: any) => UserStoreUseCase
 type Input = UserStore.Input
@@ -7,5 +8,11 @@ type Output = UserStore.Output
 export type UserStoreUseCase = (input: Input) => Promise<Output>
 
 export const setupUserStore: Setup = (firebase) => async input => {
+  const auth = getAuth(firebase)
+
+  const user: UserCredential = await createUserWithEmailAndPassword(auth, input.email, input.password)
+
+  console.log(user)
+
   return { message: 'pong' }
 }
